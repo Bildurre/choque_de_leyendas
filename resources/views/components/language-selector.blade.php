26be726b715b824ec35adfbc372b46c6a9daf95c
Bildurre <egoiastiz@gmail.com>
2025-05-05 11:38:57 +0200

    lang selector styles

--- resources/views/components/language-selector.blade.php
+++ resources/views/components/language-selector.blade.php
@@ -1,18 +1,10 @@
-<div x-data="{ open: false }" class="language-selector">
-  <button @click="open = !open" type="button" class="language-current">
-    {{ strtoupper(app()->getLocale()) }}
-    <span class="language-icon">
-      <x-core.icon name="chevron-down" />
-    </span>
-  </button>
-  
-  <div x-show="open" @click.away="open = false" class="language-dropdown">
-    @foreach(config('app.available_locales', ['es']) as $locale)
-      @if($locale !== app()->getLocale())
-        <a href="{{ route('language.change', $locale) }}" class="language-option">
-          {{ strtoupper($locale) }} - {{ locale_name($locale) }}
-        </a>
-      @endif
-    @endforeach
-  </div>
+<div class="language-selector">
+  @foreach(config('app.available_locales', ['es']) as $locale)
+    <a 
+      href="{{ route('language.change', $locale) }}" 
+      class="language-button {{ app()->getLocale() === $locale ? 'is-active' : '' }}"
+    >
+      {{ strtoupper($locale) }}
+    </a>
+  @endforeach
 </div>
