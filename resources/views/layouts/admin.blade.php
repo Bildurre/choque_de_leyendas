51c4b69bf94b75714498c90a036fcc62e5f811b5
Bildurre <egoiastiz@gmail.com>
2025-05-19 10:41:56 +0200

    attribute icons

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Alanda - Choque de Leyendas') }} - Admin</title>
+  <title>{{ config('app.name', 'Alanda - Choque de Leyendas') }}</title>
 
   <!-- Theme Script (debe ir antes de los estilos) -->
   <script>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Alanda - Choque de Leyendas') }}</title>
 
         <!-- Scripts -->
         @vite(['resources/scss/app.scss', 'resources/js/app.js'])
