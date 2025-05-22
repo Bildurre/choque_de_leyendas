977fb59c73c4b662b3141f73bb91a56312bcf611
Bildurre <egoiastiz@gmail.com>
2025-05-22 23:03:02 +0200

    lang selector 2 clicks bug

--- resources/views/components/language-selector.blade.php
+++ resources/views/components/language-selector.blade.php
@@ -2,23 +2,14 @@
 
 <div class="language-selector">
   @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-    @if($changeLocaleOnly)
-      <form action="{{ route('set-locale') }}" method="POST" class="locale-form">
-        @csrf
-        <input type="hidden" name="locale" value="{{ $localeCode }}">
-        <input type="hidden" name="redirect_url" value="{{ url()->current() }}">
-        <button type="submit" 
-          class="language-button {{ App::getLocale() == $localeCode ? 'is-active' : '' }}">
-            {{ strtoupper($localeCode) }}
-        </button>
-      </form>
-    @else
-      <a href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}" 
-         class="language-button {{ App::getLocale() == $localeCode ? 'is-active' : '' }}"
-         rel="alternate" 
-         hreflang="{{ $localeCode }}">
+    <form action="{{ route('set-locale') }}" method="POST" class="locale-form">
+      @csrf
+      <input type="hidden" name="locale" value="{{ $localeCode }}">
+      <input type="hidden" name="redirect_url" value="{{ !$changeLocaleOnly ? LaravelLocalization::getLocalizedURL($localeCode) : url()->current(); }}">
+      <button type="submit" 
+        class="language-button {{ App::getLocale() == $localeCode ? 'is-active' : '' }}">
           {{ strtoupper($localeCode) }}
-      </a>
-    @endif
+      </button>
+    </form>
   @endforeach
 </div>
